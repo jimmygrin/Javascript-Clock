d17d4fdcd837ab8394ce19ed91b59c6ba6524932
Jimmy Lin <jimmydlin@gmail.com>
2019-10-21 14:58:34 -0700

    clock v1

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
 $(document).ready(function(){
 
+
 function clock() {
 var current = new Date ();
 var hours = current.getHours();
 var minutes = current.getMinutes();
 var seconds = current.getSeconds();
+var ampm= "am"
 
-console.log(hours,minutes,seconds)
+if (seconds < 10) {
+    seconds='0'+seconds
+} else {
+    seconds=seconds
+}
+
+if (hours == 0) {
+    hours = 12
+} 
+
+if (hours > 12) {
+    hours = hours - 12;
+    ampm = "pm"
+}
+
+if (hours < 10) {
+    hours = "0" + hours
+}
+
+if (minutes < 10) {
+    minutes = "0" + minutes
+}
 
 
+var newtime = hours +':'+minutes+':'+seconds + " "+ ampm
+
+
+
+
+
+
+$("#clock").html(newtime)
 }
 
 setInterval(clock, 1000)
@@ -21,4 +52,3 @@ setInterval(clock, 1000)
 
 })
 
-// document.getElementById("clock")
